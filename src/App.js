2dfc558139b78ac7011ab2c16d0ab62cb076ff91
xenturion111 <rezaaditiyam@gmail.com>
2022-03-09 20:03:26 +0700

    commit pertama

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+
 import './App.css';
 
-function App() {
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      rates: {}
+    };
+  }
+
+  const {currencies, setCurrencies} = this.useState({});
+  useEffect(() => {
+
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
     </div>
   );
-}
+    
+ }
 
 export default App;
